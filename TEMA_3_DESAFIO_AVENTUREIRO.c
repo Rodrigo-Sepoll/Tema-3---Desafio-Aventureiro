b00b54f45d98863b55a0903b8f43238d46ee5c30
Rodrigo-Sepoll <rodrigo.sepoll@gmail.com>
2025-10-05 14:46:36 +0000

    Declaração de variáveis para a Carta 1

--- TEMA_3_DESAFIO_AVENTUREIRO.c
+++ TEMA_3_DESAFIO_AVENTUREIRO.c
@@ -4,6 +4,16 @@
 
 int main(){
      
-    printf("TEMA 3 - Desafio Super Trunfo C - Nível Aventureiro!\n\n\");
-        
+    printf("TEMA 3 - DesafioSuper Trunfo C - Nível Aventureiro!\n\n");
+
+    //Declaração de variáveis para a Carta 1
+    int Carta1[20];
+    char Nome_do_Pais1[20];
+    int Populacao1;
+    float Area1;
+    float PIB1;
+    int Pontos_Turisticos1;
+    float Densidade_Demografica1;
+
+
 }
